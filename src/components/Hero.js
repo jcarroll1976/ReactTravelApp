2acde101d127e0dfdfc8d08ff8d84d5a02609166
Joshua Carroll <carrolljosh1976@gmail.com>
2022-11-12 18:03:10 -0500

    Initialized React Travel App

---
+++ src/components/Hero.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import "./Hero.css";
+import Video from "../assets/maldivesVideo.mp4";
+
+function Hero() {
+  return (
+    <div className="hero">
+        <video autoPlay loop muted id="video">
+            <source src={Video} type="video/mp4" />
+        </video>
+        <div className="overlay"></div>
+    </div>
+  )
+}
+
+export default Hero
