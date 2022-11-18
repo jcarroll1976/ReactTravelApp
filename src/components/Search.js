2cecd4a7760a58f979c508d9b065fb8c5f2a9c5c
Joshua Carroll <carrolljosh1976@gmail.com>
2022-11-18 14:19:14 -0500

    changed content in parapraph tag in Search component

--- src/components/Search.js
+++ src/components/Search.js
@@ -8,7 +8,7 @@ function Search() {
         <div className="container">
             <div className="left">
                 <h2>LUXURY VACATIONS FOR TWO PEOPLE</h2>
-                <p>This is some random text.This is some random text.This is some random text.This is some random text.This is some random text.This is some random text.This is some random text.</p>
+                <p>Come experience the very pinnacle of luxury Caribbean all-inclusive vacations for couples at Sandals Resorts. Our luxury beach resorts, set along the most gorgeous tropical settings and exquisite beaches in Saint Lucia, Jamaica, Antigua, The Bahamas, Grenada, Barbados and Curaçao, feature unlimited gourmet dining, unique bars serving premium liquors and wines, and every land and water sport, including complimentary green fees at our golf resorts and PADI® certified scuba diving at most resorts. If you are planning a wedding, Sandals is the leader in Caribbean destination weddings and honeymoon packages.</p>
                 <div className="search-col-2">
                     <div className="box">
                         <div>
